3bd0f1c67f2dc761e73a3d983cfd723544071db5
somilnema <somilnema29@gmail.com>
2025-07-20 00:54:12 +0530

    fix: wrap Payment in Suspense and mark ass client

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -1,15 +1,12 @@
-'use client';
-import dynamic from 'next/dynamic';
-import { Suspense } from 'react';
+'use client'
 
-const Payment = dynamic(() => import('../components/Payment'), {
-  ssr: false, // disables server-side rendering
-});
+import React, { Suspense } from 'react'
+import Payment from '../components/Payment'
 
 export default function PaymentPage() {
   return (
-    <Suspense fallback={<div>Loading Payment...</div>}>
+    <Suspense fallback={<div className="p-10 text-center text-gray-500">Loading payment...</div>}>
       <Payment />
     </Suspense>
-  );
+  )
 }
